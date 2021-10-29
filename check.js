f7d398e97e1bafcef974d5f69ee8dea56538267e
Takada-T <90447778+Takada-T@users.noreply.github.com>
2021-10-29 20:30:49 +0900

    Update check.js

--- check.js
+++ check.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var url = require('url');
 
@@ -6,5 +7,22 @@ console.log('Start');
 var server = http.createServer(function(req, res) {
   console.log('Application started');
 });
-
+*/
 //sserver.listen(8000);
+
+console.log('1');
+
+var request = require('request');
+
+console.log('2');
+
+request.post('/', function(error, response, body){
+
+  console.log('3');
+
+  if (!error && response.statusCode == 200) {
+    console.log(body.name);
+  } else {
+    console.log('error: '+ response.statusCode);
+  }
+});
